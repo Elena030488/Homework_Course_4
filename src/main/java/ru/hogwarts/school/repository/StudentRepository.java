dea9a6e31ada27a5ebfda8a01adfce0e229c78dc
Lenka <aruru.goddes@gmail.com>
2024-07-09 18:27:47 +0300

    Исправление домашнего задания

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -9,15 +9,15 @@ import java.util.Collection;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
     Collection<Student> findByAgeBetween(Integer minAge, Integer maxAge);
-    @Query(value = "SELECT COUNT(*)" + "FROM student",
+    @Query(value = "SELECT COUNT(*) FROM student",
             nativeQuery = true)
     int getCount();
 
-    @Query(value = "SELECT AVG (age)" + "FROM student",
+    @Query(value = "SELECT AVG (age) FROM student",
             nativeQuery = true)
     double getAvgAge();
 
-    @Query(value = "SELECT *" + "FROM student" + "ORDER BY id DESC" + "LIMIT 5",
+    @Query(value = "SELECT * FROM student ORDER BY id DESC LIMIT 5",
             nativeQuery = true)
     Collection<Student> getLastFiveOrderByIdDesc();
 }
